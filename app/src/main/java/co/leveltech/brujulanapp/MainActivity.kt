d5fdf073afe78c0d937abe4c1ae40337a623008a
igorshyian <yourbestigor@gmail.com>
2023-10-06 12:20:16 +0300

    Initialize Situm library

--- app/src/main/java/co/leveltech/brujulanapp/MainActivity.kt
+++ app/src/main/java/co/leveltech/brujulanapp/MainActivity.kt
@@ -1,9 +1,15 @@
 package co.leveltech.brujulanapp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import co.leveltech.brujula.Brujula
 import co.leveltech.brujula.data.Area
 import co.leveltech.brujula.data.Prize
@@ -11,10 +17,20 @@ import co.leveltech.brujula.listener.OnBrujulaListener
 import co.leveltech.brujulan.R
 
 class MainActivity : AppCompatActivity() {
-
     private val textLog by lazy {
         findViewById<TextView>(R.id.text_log)
     }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { granted: Map<String, Boolean> ->
+            if (granted.containsValue(false).not()) {
+                startLocationUpdates()
+            } else {
+                Log.d(TAG, "Location permission not granted")
+            }
+        }
 
     private val listener = object : OnBrujulaListener {
         override fun onEnterArea(area: Area) {
@@ -34,6 +50,35 @@ class MainActivity : AppCompatActivity() {
         Brujula.getInstance().addOnBrujulaListener(listener)
 
         initClick()
+        startLocationUpdates()
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun startLocationUpdates() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_SCAN
+            ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Brujula.getInstance().startPositioning()
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.BLUETOOTH_SCAN,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    )
+                )
+            }
+        }
     }
 
     private fun initClick() {
--- brujula/src/main/java/co/leveltech/brujula/Brujula.kt
+++ brujula/src/main/java/co/leveltech/brujula/Brujula.kt
@@ -1,11 +1,45 @@
 package co.leveltech.brujula
 
 import android.content.Context
+import android.util.Log
 import co.leveltech.brujula.data.Area
 import co.leveltech.brujula.listener.OnBrujulaListener
+import es.situm.sdk.SitumSdk
+import es.situm.sdk.error.Error
+import es.situm.sdk.location.LocationListener
+import es.situm.sdk.location.LocationRequest
+import es.situm.sdk.location.LocationStatus
+import es.situm.sdk.model.location.Location
 
 class Brujula {
     private var listener: OnBrujulaListener? = null
+    private val locationRequest: LocationRequest by lazy {
+        LocationRequest.Builder().build()
+    }
+
+    private val locationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Log.d("Brujula", location.toString())
+            listener?.onEnterArea(Area(location.coordinate.latitude, location.coordinate.longitude, "Area from Gps"))
+        }
+
+        override fun onStatusChanged(locationStatus: LocationStatus) {
+            Log.d("Brujula", locationStatus.toString())
+
+        }
+
+        override fun onError(error: Error) {
+            Log.d("Brujula", error.toString())
+        }
+    }
+
+    fun startPositioning() {
+        SitumSdk.locationManager().requestLocationUpdates(locationRequest, locationListener)
+    }
+
+    fun stopPositioning() {
+        SitumSdk.locationManager().removeUpdates()
+    }
 
     fun getNearestAreas(): List<Area> {
         return nearestAreas
@@ -13,6 +47,7 @@ class Brujula {
 
     fun addOnBrujulaListener(listener: OnBrujulaListener) {
         this.listener = listener
+
     }
 
     private val nearestAreas = listOf(
@@ -42,6 +77,8 @@ class Brujula {
         ) {
             fun build() {
                 instance = Brujula()
+                SitumSdk.init(context)
+                SitumSdk.configuration().isUseRemoteConfig = true
             }
         }
     }
